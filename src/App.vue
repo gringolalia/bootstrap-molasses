72e10fcda3460f97d3bb3f8ecbc2255d08e6263a
Colin Brayton <gringolalia@gmail.com>
2019-08-06 13:14:04 -0300

    and committed to boot

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template lang="pug">
+#app
+	.bmd-layout-container.bmd-drawer-f-l.bmd-drawer-overlay(style="position: absolute")
+		header.bmd-layout-header
+			.navbar.navbar-light.bg-faded
+				button.navbar-toggler(type='button', data-toggle='drawer', data-target='#dw-s2')
+					span.sr-only Toggle drawer
+
+					i.material-icons menu
+
+				ul.nav.navbar-nav
+					li.nav-item.dignity.mr-5(style="font-size: 20px")
+						i.fa.fa-gears.fa-2x.mr-3
+						| Engenho Novo LLC
+
+		#dw-s2.bmd-layout-drawer.bg-faded
+			header.p-3.mb-2.text-white.bg-dark.text-left
+				a.navbar-brand.dignity
+					i.material-icons.mr-3 restaurant
+					| O Cardápio
+
+			ul.list-group.dignity
+				a.list-group-item
+					i.material-icons local_dining
+					| Enchiladas
+
+				a.list-group-item
+					i.material-icons room_service
+					| Tacos
+
+				a.list-group-item.active.text-white
+					i.material-icons restaurant_menu
+					| Tamales
+
+				a.list-group-item
+					i.material-icons fastfood
+					| Quesadillas
+
+				a.list-group-item
+					i.material-icons local_bar
+					| Margaritas
+
+				a.list-group-item
+					i.material-icons beach_access
+					| Cochinita
+
+				a.list-group-item
+					i.fa.fa-street-view.fa-2x
+					| Chilaquiles
+
+				a.list-group-item
+					i.material-icons free_breakfast
+					| Guacamole
+
+				a.list-group-item
+					i.fa.fa-bus.fa-lg
+					| Mole Poblano
+
+				a.list-group-item
+					i.material-icons waves
+					| Cabrito
+
+
+		main.bmd-layout-content
+			.container
+				router-view
+</template>
+
+<script>
+export default {
+	name: 'App'
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+
+}
+
+.list-group-item.active {
+	background-color: #607D8B;
+	color: white;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.hello.mt-5
+  img(src="../assets/hammer-sickle.svg" width="150px")
+  h1 {{ msg }}
+  h2 Bootstrap Material Design
+  h4 With Vueter Router to Booter
+  button.btn.btn-secondary.btn-lg.active.mt-5.dignity
+    i.fa.fa-street-view.fa-2x.mr-2
+    | Impress Me
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Adeus, Mundo Imundo'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
